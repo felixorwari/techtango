841a452d48466c60a753a23795bf7491d124ee27
Felix Orwari <felix.orwari@gmail.com>
2024-05-15 23:18:39 +0300

    First commit

---
+++ src/views/EventsView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import EventComponent from '../components/EventComponent.vue'
+</script>
+
+<template>
+  <main>
+    <section class="prose">
+      <header>
+        <h1>Upcoming Events</h1>
+      </header>
+
+      <EventComponent />
+    </section>
+
+    <section class="prose">
+      <header>
+        <h1>Past Events</h1>
+      </header>
+    </section>
+  </main>
+</template>
