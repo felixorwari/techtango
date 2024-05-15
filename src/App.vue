7d5c1b6cd039523bf6219c9f0f57d9a5684086de
Felix Orwari <felix.orwari@gmail.com>
2024-05-15 23:58:15 +0300

    Refactored general layout & links

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,50 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <div class="wrapper">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">Events</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
       </nav>
     </div>
   </header>
 
   <RouterView />
+
+  <footer class="py-12 mt-auto border-t border-gray-100">
+    <div class="w-full max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm sm:text-center"
+        >&copy; 2024 <RouterLink to="/" class="hover:underline">Tech Tango</RouterLink>. All rights
+        reserved.</span
+      >
+
+      <ul class="flex flex-wrap items-center mt-3 text-sm sm:mt-0">
+        <li>
+          <RouterLink
+            to="/about"
+            class="transition hover:underline hover:underline-offset-4 me-4 md:me-6"
+            >About</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink
+            to="/privacy"
+            class="transition hover:underline hover:underline-offset-4 me-4 md:me-6"
+            >Privacy Policy</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink
+            to="/terms"
+            class="transition hover:underline hover:underline-offset-4 me-4 md:me-6"
+            >Terms of Service</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink to="/contact" class="transition hover:underline hover:underline-offset-4"
+            >Contact</RouterLink
+          >
+        </li>
+      </ul>
+    </div>
+  </footer>
 </template>
