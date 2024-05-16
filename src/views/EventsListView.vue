c5ba75b9f4d64adfa19d7efc4d144b9f68bafe8d
Felix Orwari <felix.orwari@gmail.com>
2024-05-16 20:00:41 +0300

    Created EventService

--- src/views/EventsListView.vue
+++ src/views/EventsListView.vue
@@ -1,13 +1,12 @@
 <script setup>
-import EventCardComponent from '@/components/EventCardComponent.vue'
 import { ref, onMounted } from 'vue'
-import axios from 'axios'
+import EventCardComponent from '@/components/EventCardComponent.vue'
+import EventService from '@/services/EventService'
 
 const events = ref(null)
 
 onMounted(() => {
-  axios
-    .get('http://localhost:3000/events?_sort=category')
+  EventService.getEvents()
     .then((response) => {
       events.value = response.data
     })
@@ -20,17 +19,13 @@ onMounted(() => {
 <template>
   <main>
     <section class="">
-      <h1 class="sticky top-0 z-30 py-8 mb-6 text-2xl font-bold text-center bg-white">
-        Today's Events
-      </h1>
+      <h1 class="py-8 mb-6 text-2xl font-bold text-center bg-white">Today's Events</h1>
 
       <div class="flex flex-col space-y-10"></div>
     </section>
 
     <section class="">
-      <h1 class="sticky top-0 z-30 py-8 mb-6 text-2xl font-bold text-center bg-white">
-        Upcoming Events
-      </h1>
+      <h1 class="py-8 mb-6 text-2xl font-bold text-center bg-white">Upcoming Events</h1>
 
       <div class="flex flex-col space-y-10">
         <EventCardComponent v-for="event in events" :key="event.id" :event="event" />
