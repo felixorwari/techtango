bfc70a50608d6527e97685223f82693a67a60d8f
Felix Orwari <felix.orwari@gmail.com>
2024-05-16 18:12:44 +0300

    Updated layout

--- src/components/EventCardComponent.vue
+++ src/components/EventCardComponent.vue
@@ -1,4 +1,20 @@
-<script setup></script>
+<script setup>
+defineProps({
+  event: {
+    type: Object,
+    required: true
+  }
+})
+</script>
 <template>
-  <div></div>
+  <RouterLink to="/events">
+    <div
+      class="px-6 py-10 transition-all bg-white hover:bg-gray-50 border border-gray-300 rounded shadow-lg hover:scale-[102%] prose max-w-none"
+    >
+      <h3>{{ event.title }}</h3>
+
+      <p>{{ event.time }} on {{ event.date }}</p>
+      <p>@ {{ event.location }}</p>
+    </div>
+  </RouterLink>
 </template>
