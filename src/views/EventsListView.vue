5969b0ece785148ede1e07ed30043438e72fa8ad
Felix Orwari <felix.orwari@gmail.com>
2024-05-16 22:56:52 +0300

    Updated events list UI

--- src/views/EventsListView.vue
+++ src/views/EventsListView.vue
@@ -19,13 +19,7 @@ onMounted(() => {
 <template>
   <main>
     <section class="">
-      <h1 class="py-8 mb-6 text-2xl font-bold text-center bg-white">Today's Events</h1>
-
-      <div class="flex flex-col space-y-10"></div>
-    </section>
-
-    <section class="">
-      <h1 class="py-8 mb-6 text-2xl font-bold text-center bg-white">Upcoming Events</h1>
+      <h1 class="pb-8 mb-6 text-2xl font-bold text-center bg-white">Upcoming Events</h1>
 
       <div class="flex flex-col space-y-10">
         <EventCardComponent v-for="event in events" :key="event.id" :event="event" />
