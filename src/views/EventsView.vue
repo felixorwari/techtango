e39799c3995f15bac786bb49a8169e9ab632d06d
Felix Orwari <felix.orwari@gmail.com>
2024-05-16 18:14:20 +0300

    Added data fetching

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,31 +1,33 @@
 <script setup>
 import EventCardComponent from '@/components/EventCardComponent.vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const events = ref(null)
+
+onMounted(() => {
+  axios
+    .get('http://localhost:3000/events?_sort=category')
+    .then((response) => {
+      events.value = response.data
+      console.log(events)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
 </script>
 
 <template>
   <main>
     <section class="">
       <header class="prose">
-        <h1>Today's Events</h1>
-      </header>
-
-      <EventCardComponent />
-    </section>
-
-    <section class="">
-      <header class="prose">
         <h1>Upcoming Events</h1>
       </header>
 
-      <EventCardComponent />
-    </section>
-
-    <section class="">
-      <header class="prose">
-        <h1>Past Events</h1>
-      </header>
-
-      <EventCardComponent />
+      <div class="flex flex-col space-y-8">
+        <EventCardComponent v-for="event in events" :key="event.id" :event="event" />
+      </div>
     </section>
   </main>
 </template>
