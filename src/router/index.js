ecb93b7ed9abd80fe098d4a2216c2a0c9c1443ad
Felix Orwari <felix.orwari@gmail.com>
2024-05-18 18:05:52 +0300

    Changed import paths to absolute

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import EventsListView from '../views/event/ListView.vue'
-import EventDetailsView from '../views/event/DetailsView.vue'
-import AboutView from '../views/AboutView.vue'
-import ContactView from '../views/ContactView.vue'
-import PrivacyView from '../views/PrivacyView.vue'
-import TermsView from '../views/TermsView.vue'
-import UserView from '../views/user/ProfileView.vue'
-import events from '../stores/events.json'
+import EventsListView from '@/views/event/ListView.vue'
+import EventDetailsView from '@/views/event/DetailsView.vue'
+import AboutView from '@/views/AboutView.vue'
+import ContactView from '@/views/ContactView.vue'
+import PrivacyView from '@/views/PrivacyView.vue'
+import TermsView from '@/views/TermsView.vue'
+import UserView from '@/views/user/ProfileView.vue'
+import events from '@/stores/events.json'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
